d6be7d354899a0cdfb76fabe983e738f63dbb764
Fabio Bione <fabiolopesbione@hotmail.com>
2020-02-23 14:57:38 -0300

    Referenciando os posts do usuário

--- src/main/java/com/bios/domain/User.java
+++ src/main/java/com/bios/domain/User.java
@@ -1,8 +1,11 @@
 package com.bios.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value="user")
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		super();
@@ -74,6 +80,14 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 	
 }
